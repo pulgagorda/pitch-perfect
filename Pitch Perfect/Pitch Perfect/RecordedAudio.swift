3232bb394a1655680d328c31bc1f01fd6f919f9d
pulgagorda <patriveri@gmail.com>
2015-04-04 10:51:30 -0700

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Patricio Triveri on 3/25/15.
+//  Copyright (c) 2015 patriciotriveri. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    // parameters passed from RecordSoundsViewController to PlaySoundsViewController
+    var filePathUrl: NSURL!
+    var title: String!
+}
